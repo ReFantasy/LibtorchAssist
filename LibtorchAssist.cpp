d7f42e968ff63815955a54ca8d4be83a85d313bc
TDL <309741309@qq.com>
2020-07-11 19:35:32 +0800

    initial

---
+++ LibtorchAssist.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TDL on 2020/7/11.
+//
+
+#include "LibtorchAssist.h"
+#include <iostream>
+int main()
+{
+	std::cout<<"ok"<<std::endl;
+}
---
+++ LibtorchAssist.h
@@ -0,0 +1,8 @@
+//
+// Created by TDL on 2020/7/11.
+//
+
+#ifndef __MNIST_LIBTORCHASSIST_H__
+#define __MNIST_LIBTORCHASSIST_H__
+
+#endif//__MNIST_LIBTORCHASSIST_H__
